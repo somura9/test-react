56b84327ebc6297c2d32b1314105d6887b588f94
somura asuka <104813987+somura9@users.noreply.github.com>
2023-03-28 17:14:40 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    // キャメルケースで記述, 文字列で囲う
+    fonstSize: "18px"
+  };
+
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>保存し忘れてた</p>
+      {/* ={}でJavaScriptのオブジェクトを書く */}
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>保存し忘れてた</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
