8c9ee9e5c22ab9a8b94b2cb0a0626be53f801916
somura asuka <104813987+somura9@users.noreply.github.com>
2023-03-28 16:57:59 +0900

    Revert "コンポーネント"

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import React from "react";
 // コンポーネントを表示するライブラリをインポート
 import ReactDom from "react-dom";
 
-//コンポーネントをインポート
-import App from "./App.jsx";
+const App = () => {
+  return (
+    // returnする内容は一つのタグで囲わないといけない
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか</p>
+    </>
+  );
+};
 
 ReactDom.render(<App />, document.getElementById("root"));
 //jsのなかでreturnしてhtmlを書いていくこれらの記法をJSX記法という
